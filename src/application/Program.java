9fccace0ee2e1e00713a55c44e79e4dc35dabd13
André Barcelos Michels de Mattos <112213284+abmMattos@users.noreply.github.com>
2023-06-07 23:33:45 -0300

    Primeiro commit
    Primeiro commit

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package src.application;
+
+import src.chess.ChessMatch;
+
+public class Program{
+    public static void main(String[] args) {
+    
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+    }
+} 
